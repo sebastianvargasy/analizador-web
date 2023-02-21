c034d8913c4d7ad2adbb068ad968d97aa0bc61f8
sebastianvargasy <64619819+sebastianvargasy@users.noreply.github.com>
2023-02-21 13:38:19 -0300

    Update index.py

--- index.py
+++ index.py
@@ -15,5 +15,18 @@ tree = html.fromstring(response.content)
 # Por ejemplo, el título de la página:
 title = tree.findtext('.//title')
 
+# Extraer otros elementos de la página web
+header = tree.findtext('.//h1')
+paragraphs = tree.findall('.//p')
+
+# Extraer información de otras páginas del sitio web
+about_url = 'https://sochisi.cl/directorio/'
+about_response = requests.get(about_url)
+about_tree = html.fromstring(about_response.content)
+about_header = about_tree.findtext('.//h1')
+
 # Mostrar el resultado en la aplicación de Streamlit
 st.write(f"El título de la página es: {title}")
+st.write(f"El encabezado de la página es: {header}")
+st.write(f"Los párrafos de la página son: {paragraphs}")
+st.write(f"El encabezado de la página 'Acerca de' es: {about_header}")
